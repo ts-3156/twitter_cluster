f1ccc60d5feacee5c23b6d177ca2ca169d506454
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-02 19:45:18 +0900

    Initial commit

---
+++ lib/twitter_cluster.rb
@@ -0,0 +1,6 @@
+require "twitter_cluster/version"
+
+module TwitterCluster
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/twitter_cluster/version.rb
@@ -0,0 +1,3 @@
+module TwitterCluster
+  VERSION = "0.1.0"
+end
